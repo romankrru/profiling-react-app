e4851eb5cc2139872cbf86cb5f2b38cebe7c6ae0
romankrru <rm07ru@gmail.com>
2018-11-09 14:13:09 +0300

    Pure component refactor

--- src/FlameChart.js
+++ src/FlameChart.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {PureComponent} from 'react';
 import {FixedSizeList as List} from 'react-window';
 import {convertData} from './utils';
 
@@ -6,33 +6,49 @@ export default function FlameChart ({data, width, height}) {
 	const listData = convertData(data);
 
 	return (
-		<List height={height} itemCount={listData.length} itemSize={35} width={width}>
-			{({index, style}) => {
-				const nodes = listData[index];
+		<List
+			height={height}
+			itemCount={listData.length}
+			itemSize={35}
+			width={width}
 
-				return <div style={style}>
-					{nodes.map((node, index) => (
-						<div
-							key={index}
-							style={{
-								position: 'absolute',
-								left: node.offset * width,
-								width: node.width * width,
-								height: 30,
-								lineHeight: '30px',
-								backgroundColor: node.color,
-								border: '1px solid white',
-								boxSizing: 'border-box',
-								textOverflow: 'ellipsis',
-								overflow: 'hidden'
-							}}
-							title={node.name}
-						>
-							{node.name}
-						</div>
-					))}
-				</div>
+			itemData={{
+				listData,
+				width
 			}}
+		>
+			{ListItem}
 		</List>
 	)
+}
+
+class ListItem extends PureComponent {
+	render() {
+		const {data, index, style} = this.props;
+		const {listData, width} = data
+		const nodes = listData[index];
+
+		return <div style={style}>
+			{nodes.map((node, index) => (
+				<div
+					key={index}
+					style={{
+						position: 'absolute',
+						left: node.offset * width,
+						width: node.width * width,
+						height: 30,
+						lineHeight: '30px',
+						backgroundColor: node.color,
+						border: '1px solid white',
+						boxSizing: 'border-box',
+						textOverflow: 'ellipsis',
+						overflow: 'hidden'
+					}}
+					title={node.name}
+				>
+					{node.name}
+				</div>
+			))}
+		</div>
+	}
 }
